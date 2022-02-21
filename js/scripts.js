837c9b371d58bd9f00e35b86c10c48ad0414b6b6
Yaroslav Russkih <pseudoyar@gmail.com>
2022-02-21 22:41:42 +0700

    замечания

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
-/*!
-* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Set the date we're counting down to
+var countDownDate = new Date("Aug 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="countDown"
+  document.getElementById("countDown").innerHTML = days + " дней " + hours + " часов "
+  + minutes + " минут " + seconds + " секунд ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countDown").innerHTML = "Тут ссылка на регистрацию";
+  }
+}, 1000);
